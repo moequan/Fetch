1baeea4dc6cf30983557e43e66df69f2dd44f0cd
moequan <maurice.s93@web.de>
2019-11-25 09:48:48 +0100

    Starting with Chaining.

---
+++ main.js
@@ -0,0 +1,26 @@
+ //Promise Chanining
+
+ let currentRuler = 'Cersei Lannister';
+ const changeRuler = (firstRuler, secondRuler, thirdRuler, fourthRuler, finalRuler) => {
+     setTimeout(() => {
+         currentRuler = firstRuler;
+         console.log(currentRuler);
+         setTimeout(() => {
+             currentRuler = secondRuler;
+             console.log(currentRuler);
+             setTimeout(() => {
+                 currentRuler = thirdRuler;
+                 console.log(currentRuler);
+                 setTimeout(() => {
+                     currentRuler = fourthRuler;
+                     console.log(currentRuler);
+                     setTimeout(() => {
+                         currentRuler = finalRuler;
+                         console.log(currentRuler);
+                     }, 1000)
+                 }, 1000)
+             }, 2000)
+         }, 3000)
+     }, 5000)
+ };
+ changeRuler('Maria', 'Maurice', 'Swantje', 'Florian', 'Vladi');
