94159c55778fc4bf88f8ea8e5d164c7becf4f71f
moequan <maurice.s93@web.de>
2019-11-26 11:28:47 +0100

    Create function for POST Data to server.

--- Fetch API/main.js
+++ Fetch API/main.js
@@ -2,6 +2,8 @@ console.log("hehehehe")
 
 document.getElementById('btnGetUserData').addEventListener("click", getUserData);
 
+
+
 function getUserData() {
   console.log("Testing event");
 
@@ -10,11 +12,51 @@ function getUserData() {
     .then(users => {
       // console.log(users))
       let html = `<h2>List of users</h2>`
-      html +=`<ul>`
-      users.forEach( user => {
+      html += `<ul>`
+      users.forEach(user => {
         html += `<li>${user.name}</li>`
       })
-      html +=`</ul>`
-      document.getElementById('response').innerHTML=html
+      html += `</ul>`
+      document.getElementById('response').innerHTML = html
     })
-    }
+}
+
+//Send data to server
+
+document.getElementById('addPostForm').addEventListener("submit", addPost)
+
+function addPost(e) {
+  e.preventDefault();
+
+  let title = document.getElementById("title").value;
+  let body = document.getElementById("body").value;
+
+  const myPost = {
+    title: title,
+    body: body
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(myPost)
+  };
+  fetch("https://jsonplaceholder.typicode.com/posts", options)
+    .then(response => {
+      if (response.ok) {
+
+        return response.json();
+      } else {
+        return Promise.reject({
+          status: reponse.status,
+          statusText: responseStatusText
+        });
+      }
+    })
+    .then((data) => console.log(data))
+    .catch(err => {
+      console.log("Error message:" + err)
+    })
+
+}
